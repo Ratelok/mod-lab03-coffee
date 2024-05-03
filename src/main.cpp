04faa94797c68d361954e8b31691c00067bcd727
Ratelok <120968192+Ratelok@users.noreply.github.com>
2024-05-03 13:51:16 +0300

    Федоров Никита 382007-3

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
-// #include "Automata.h"
+#include "Automata.h"
 
-// int main() {
-//     Automata automata;
-//     automata.on();
-//     automata.coin();
-//     automata.coin();
-//     automata.etMenu();
-//     automata.choice();
-//     automata.check();
-//     automata.cook();
-//     automata.finish();
-//     automata.off();
-//     return 0;
-// }
+int main() {
+    Automata automata;
+    automata.on();
+    automata.coin();
+    automata.coin();
+    automata.etMenu();
+    automata.choice();
+    automata.check();
+    automata.cook();
+    automata.finish();
+    automata.off();
+    return 0;
+}
